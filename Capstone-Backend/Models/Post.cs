463a21428adaf372a8c378f2a6c7c23ab2bff050
Zachary <taylorzw96@gmail.com>
2021-08-18 14:41:19 -0500

    Adding Post Model

---
+++ Capstone-Backend/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone_Backend.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public User User { get; set; }
+        [ForeignKey("Topic")]
+        public int TopicId { get; set; }
+        public Topic Topic { get; set; }
+        public int DateCreated { get; set; }
+    }
+}
