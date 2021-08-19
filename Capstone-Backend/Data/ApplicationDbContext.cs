441914c7f823be52b674e9cb2447070963b62e28
Zachary <taylorzw96@gmail.com>
2021-08-18 21:45:55 -0500

    Adding DbContext file, AppSettings, and modified Startup.cs while remmoving controllers

---
+++ Capstone-Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+using Capstone_Backend.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Capstone_Backend.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        // Create DbSets here for adding new tables to the database
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //    modelBuilder.ApplyConfiguration(new RolesConfiguration());
+        }
+
+    }
+}
--- Capstone-Backend/Startup.cs
+++ Capstone-Backend/Startup.cs
@@ -1,7 +1,9 @@
+using Capstone_Backend.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,6 +16,7 @@ using System.Threading.Tasks;
 
 namespace Capstone_Backend
 {
+   {
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -26,12 +29,8 @@ namespace Capstone_Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Capstone_Backend", Version = "v1" });
-            });
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("sqlConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +39,6 @@ namespace Capstone_Backend
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Capstone_Backend v1"));
             }
 
             app.UseHttpsRedirection();
