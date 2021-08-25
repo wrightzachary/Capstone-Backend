bf164897feb135d6af8788f8dcb8785752861287
Zachary <taylorzw96@gmail.com>
2021-08-25 09:43:07 -0500

    Adding Psot model

---
+++ CapstoneBackend/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneBackend.Models 
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public string User { get; set; }
+        [ForeignKey("Topic")]
+        public int TopicId { get; set; }
+        public string Topic { get; set; }
+        public string PostName { get; set; }
+        public int DateCreated { get; set; }
+    }
+}
